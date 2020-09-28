12ef7598a7594430cb5b805e735127ebd7dbf4df
Michel Vollebregt <mi.vollebregt@gmail.com>
2020-09-28 16:01:58 +0200

    Initial Commit

---
+++ lampaan/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  lampaan
+//
+//  Created by Michel Vollebregt on 2020-09-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
